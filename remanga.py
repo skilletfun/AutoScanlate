9a823678f83dfdb299347e4b99134950e5528ee3
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-07-15 19:07:14 +0300

    Hints, docs, comments, fix Marketing-value

--- remanga.py
+++ remanga.py
@@ -7,7 +7,10 @@ from config import RANGES, ACCOUNTS
 import time
 
 
-def login_remanga(driver):
+def login_remanga(driver: Browser) -> None:
+    """ Вход в аккаунт Реманги.
+    :param driver: объект браузера
+    """
     driver.get('https://remanga.org')
     driver.execute("document.getElementsByClassName('MuiButtonBase-root')[7].click();")
     driver.send_keys_to(By.ID, 'login', ACCOUNTS['ramanga'][0])
@@ -16,13 +19,22 @@ def login_remanga(driver):
         "document.getElementsByClassName('MuiButton-containedPrimary MuiButton-containedSizeLarge')[0].click();")
     time.sleep(3)
 
-def wait(driver):
-    timer = 30
-    tries = 5
+def wait(driver: Browser) -> (str, list):
+    """ Загружает страницу, пока не появятся необходимые элементы.
+    :param driver: объект браузера
+    :return: возвращает данные для столбца Реклама и список глав
+    """
+    timer, tries = 30, 5
     while True:
         try:
+            # Если тайтл не принадлежит аккаунту, то вернется текст Читать / Прочитано / Продолжить
             status = driver.execute("return document.getElementsByClassName('MuiButton-label')[2].textContent;")
-            marketing = status.split()[-1] if 'Продвигается' in status or 'В очереди' in status else 'Продвижение'
+            if 'Продвигается' in status or 'В очереди' in status:
+                marketing = status.split()[-1]
+            elif 'Продвижение' in status:
+                marketing = 'Продвижение'
+            else:
+                marketing = '-'
             children = driver.execute("return document.getElementsByClassName('MuiGrid-grid-md-6')[0].lastChild.children;")[1:]
             children[0].find_element(By.CSS_SELECTOR, 'div:nth-child(3)')
             break
@@ -30,12 +42,11 @@ def wait(driver):
             time.sleep(0.1)
             timer -= 1
             if timer < 0:
-                timer = 30
-                tries -= 1
-                if tries < 0:
-                    return '-', '-', []
+                timer, tries = 30, tries-1
+                if tries < 0: return '-', '-', []
                 driver.driver.refresh()
-    return status, marketing, children
+    return marketing, children
+
 
 if __name__ == '__main__':
     log = Logger()
@@ -46,27 +57,29 @@ if __name__ == '__main__':
     try:
         result_arr = []
         for url in urls:
-            payed, free, marketing, date = -1, -1, -1, -1
+            url = url.replace('?subpath=about', '')
+            payed, free, marketing, date = '-', '-', '-', '-'
             total_payed = 0
 
-            driver.get(url.replace('?subpath=about', ''))
-
+            driver.get(url)
             if not driver.check_element(By.CLASS_NAME, 'MuiAvatar-root'):
                 login_remanga(driver)
                 driver.get(url)
 
-            status, marketing, children = wait(driver)
-
+            marketing, children = wait(driver)
             for child in children:
+                # Возвращает кусок текста, по которому точно можно сказать, платная или бесплатная глава
                 el = child.find_element(By.CSS_SELECTOR, 'div:nth-child(3)').get_attribute('innerHTML')[2:5]
+                # Дата главы
                 date = child.find_element(By.CSS_SELECTOR, 'a > div > span:nth-child(2)').get_attribute('innerHTML')
-                header = child.find_element(By.CSS_SELECTOR, 'a > h6').text.split()[1]
+                # Номер главы
+                num = child.find_element(By.CSS_SELECTOR, 'a > h6').text.split()[1]
 
                 if el.startswith('v'):
                     total_payed += 1
-                    if payed == -1: payed = f'=ГИПЕРССЫЛКА("{url}";"{header}")'
+                    if payed == '-': payed = f'=ГИПЕРССЫЛКА("{url}";"{num}")'
                 elif el.startswith('p'):
-                    free = f'=ГИПЕРССЫЛКА("{url}";"{header}")'
+                    free = f'=ГИПЕРССЫЛКА("{url}";"{num}")'
                     break
             result_arr.append([payed, free, total_payed, date, marketing])
         sheeter.write_values(result_arr, RANGES['remanga'], dimension='ROWS')
