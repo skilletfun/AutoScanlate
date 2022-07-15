ab1767632bebe773416eb04c17d12f0af74e024e
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-07-15 18:23:22 +0300

    Fix rateLimitExceeded

--- gdrive.py
+++ gdrive.py
@@ -1,13 +1,12 @@
 import httplib2
 from aiogoogle import Aiogoogle
 from aiogoogle.auth.creds import ServiceAccountCreds
-import googleapiclient.discovery
 from config import DRIVE_ID
 import asyncio
 from sheet import Sheet
-from logger import Logger
+from logger import log
 import json
-from config import RANGES
+from config import RANGES,DRIVE_FETCH_LIMIT
 
 
 class Drive:
@@ -18,42 +17,55 @@ class Drive:
     }
     TYPES = ['Перевод', 'Редакт', 'Клин', 'Тайп', 'Эдит', 'Звуки', 'Бета', 'Сканы']
 
-    def get_creds(self):
+    def __init__(self):
+        self.sem = asyncio.Semaphore(20)
+
+    async def get_creds(self):
         service_account_key = json.load(open('creds.json'))
         return ServiceAccountCreds(
             scopes=['https://www.googleapis.com/auth/drive'],
             **service_account_key
         )
 
-    async def get_files(self, title: str, genre: str) -> list[list[str], ...]:
+    @log
+    async def parse(self, names):
+        async with Aiogoogle(service_account_creds=await self.get_creds()) as aiog:
+            async with asyncio.Semaphore(DRIVE_FETCH_LIMIT):
+                return await asyncio.gather(*[self.get_files(aiog, el[0], el[1]) for el in names])
+
+    async def get_files(self, aiog, title, genre) -> list[list[str], ...]:
         """ Пробегается по папкам с тайтлами
+        :param aiog: объект, выполняющий запросы
         :param title: название тайтла
         :param genre: жанр тайтла
         :return: список со строками (ROWS) для гугл-таблицы
         """
-        async with asyncio.Semaphore(3):
-            async with Aiogoogle(service_account_creds=self.get_creds()) as aiog:
-                service = await aiog.discover("drive", "v3")
+        service = await aiog.discover("drive", "v3")
+        q_parent = f"mimeType = 'application/vnd.google-apps.folder' and name contains '{title}' and '{self.SCOPES[genre]}' in parents"
+        # Запрос 1, возвращает id папки с тайтлом
+        parent = (await aiog.as_service_account(service.files.list(fields="files(id)", q=q_parent)))['files']
+        if len(parent) > 0: parent = parent[0]['id']    # Если тайтла не нашлось, то возвращаем прочерки
+        else: return '-','-','-','-','-','-','-'
 
-                q_parent = f"mimeType = 'application/vnd.google-apps.folder' and name contains '{title}' and '{self.SCOPES[genre]}' in parents"
-                parent = (await aiog.as_service_account(service.files.list(fields="files(id)", q=q_parent)))['files']
+        await asyncio.sleep(0.5)
 
-                if len(parent) > 0: parent = parent[0]['id']    # Если тайтла не нашлось, то возвращаем прочерки
-                else: return '-','-','-','-','-','-','-',
+        q_filter = f"mimeType = 'application/vnd.google-apps.folder' and '{parent}' in parents"
+        # Запрос 2, возвращает id + name папок внутри папок с тайтлами
+        folders = (await aiog.as_service_account(service.files.list(fields="files(id, name)", q=q_filter)))['files']
 
-                q_filter = f"mimeType = 'application/vnd.google-apps.folder' and '{parent}' in parents"
-                folders = (await aiog.as_service_account(service.files.list(fields="files(id, name)", q=q_filter)))['files']
+        await asyncio.sleep(0.5)
 
-                result = {}
-                for el in self.TYPES:
-                    for folder in folders:
-                        if folder['name'].startswith(el):
-                            result[el] = await self.get_last_file(folder['id'], service, aiog)
-                            break
-                    else: result[el] = '-'
-                if result['Эдит'] != '-':
-                    result['Тайп'] = result['Эдит']
-                return [result[el] for el in self.TYPES[:4] + self.TYPES[5:]]
+        result = {}
+        for el in self.TYPES:
+            for folder in folders:
+                if folder['name'].startswith(el):
+                    # Выполнение запроса 3 для каждой папки. Максимум - 8 раз.
+                    result[el] = await self.get_last_file(folder['id'], service, aiog)
+                    break
+            else: result[el] = '-'
+        if result['Эдит'] != '-':
+            result['Тайп'] = result['Эдит']
+        return [result[el] for el in self.TYPES[:4] + self.TYPES[5:]]
 
     async def get_last_file(self, folder_id: str, service, aiog) -> str:
         """ Находит номер последней главы в папке
@@ -63,6 +75,7 @@ class Drive:
         :return: номер последней главы
         """
         q_filter = f"mimeType != 'application/vnd.google-apps.folder' and '{folder_id}' in parents"
+        # Запрос 3, возвращает список файлов в папке
         files = (await aiog.as_service_account(service.files.list(fields="files(name)", q=q_filter)))['files']
         nums = []
         for file in files:
@@ -85,14 +98,13 @@ async def main():
     sheeter = Sheet()
     drive = Drive()
     names = sheeter.get_names_with_genres()
-    drive_list = await asyncio.gather(*[drive.get_files(el[0], el[1]) for el in names])
+    await drive.get_creds()
+    drive_list = await drive.parse(names)
     sheeter.write_values(drive_list, RANGES['drive'], dimension='ROWS')
 
 if __name__ == '__main__':
-    log = Logger()
-    log.log('Start parse GDrive')
     try:
         asyncio.run(main())
-    except Exception as e:
-        log.error(e)
+    except RuntimeError:
+        pass
 
