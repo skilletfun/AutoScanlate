079e8a702fa738311e0a6adfd9534ddf4c129adf
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-07-16 16:21:13 +0300

    Fix (search folder too), refactor

--- gdrive.py
+++ gdrive.py
@@ -1,24 +1,24 @@
+import json
+import asyncio
+
 import httplib2
 from aiogoogle import Aiogoogle
 from aiogoogle.auth.creds import ServiceAccountCreds
-from config import DRIVE_ID
-import asyncio
+
 from sheet import Sheet
 from logger import log
-import json
-from config import RANGES,DRIVE_FETCH_LIMIT
+from config import RANGES, DRIVE_FETCH_LIMIT
 
 
 class Drive:
+    """ Парсер для гугл-диска. """
+
     CREDENTIALS_FILE = 'creds.json'
     SCOPES = {
         'Сёдзе': '1pYp-eYB1HDhKgMB8EjQ_xCH_EaNFe5-62zlPRoGvH_AXmnMVXqyrz4u5sLLsNnjhL0wnut5j',
         'Сёнэн': '1GQNgefS6_YXrvPRzBCcKlpHHR9Jrmr019rFZHzO6NAm_LyA9IC-g1ZCmpyfXqCnQG0NO3-Hv'
     }
     TYPES = ['Перевод', 'Редакт', 'Клин', 'Тайп', 'Эдит', 'Звуки', 'Бета', 'Сканы']
-
-    def __init__(self):
-        self.sem = asyncio.Semaphore(20)
 
     async def get_creds(self):
         service_account_key = json.load(open('creds.json'))
@@ -33,6 +33,7 @@ class Drive:
             async with asyncio.Semaphore(DRIVE_FETCH_LIMIT):
                 return await asyncio.gather(*[self.get_files(aiog, el[0], el[1]) for el in names])
 
+    @log
     async def get_files(self, aiog, title, genre) -> list[list[str], ...]:
         """ Пробегается по папкам с тайтлами
         :param aiog: объект, выполняющий запросы
@@ -42,24 +43,20 @@ class Drive:
         """
         service = await aiog.discover("drive", "v3")
         q_parent = f"mimeType = 'application/vnd.google-apps.folder' and name contains '{title}' and '{self.SCOPES[genre]}' in parents"
-        # Запрос 1, возвращает id папки с тайтлом
+        # Возвращает id папки с тайтлом
         parent = (await aiog.as_service_account(service.files.list(fields="files(id)", q=q_parent)))['files']
         if len(parent) > 0: parent = parent[0]['id']    # Если тайтла не нашлось, то возвращаем прочерки
         else: return '-','-','-','-','-','-','-'
 
-        await asyncio.sleep(0.5)
-
         q_filter = f"mimeType = 'application/vnd.google-apps.folder' and '{parent}' in parents"
-        # Запрос 2, возвращает id + name папок внутри папок с тайтлами
+        # Возвращает id и name папок внутри папок с тайтлами
         folders = (await aiog.as_service_account(service.files.list(fields="files(id, name)", q=q_filter)))['files']
-
-        await asyncio.sleep(0.5)
 
         result = {}
         for el in self.TYPES:
             for folder in folders:
                 if folder['name'].startswith(el):
-                    # Выполнение запроса 3 для каждой папки. Максимум - 8 раз.
+                    # Добавляет в словарь для каждой папки номер последней залитой главы.
                     result[el] = await self.get_last_file(folder['id'], service, aiog)
                     break
             else: result[el] = '-'
@@ -67,33 +64,34 @@ class Drive:
             result['Тайп'] = result['Эдит']
         return [result[el] for el in self.TYPES[:4] + self.TYPES[5:]]
 
+    @log
     async def get_last_file(self, folder_id: str, service, aiog) -> str:
         """ Находит номер последней главы в папке
         :param folder_id: id гугл-папки, в которой происходит поиск
         :param service: объект, через генерирует запросы
         :param aiog: объект, который выполняет запросы
-        :return: номер последней главы
+        :return: номер последней главы. Если глав не нашлось, возвращает прочерк
         """
-        q_filter = f"mimeType != 'application/vnd.google-apps.folder' and '{folder_id}' in parents"
-        # Запрос 3, возвращает список файлов в папке
+        q_filter = f"'{folder_id}' in parents"
+        # Возвращает список файлов и папок в папке.
         files = (await aiog.as_service_account(service.files.list(fields="files(name)", q=q_filter)))['files']
         nums = []
         for file in files:
             try:
+                # Если в названии есть точка (скорее всего расширение файла), то берем первую часть.
                 num = file['name'].split('.')[0]
+                # Если в названии есть дефис, то сплитим часть после него, в противном случае просто сплитим.
                 if num.find('-') > 0:
-                    num = num[num.find('-'):].split()
-                else:
-                    num = num.split()
-                for el in num:
-                    if el.isdigit():
-                        nums.append(int(el))
-                else:
-                    nums.append(0)
+                    num = num[num.find('-'):]
+                num = num.split()
+                # Пробегаемся по получившемуся списку. Когда найдем число, добавляем его в список.
+                [nums.append(int(el)) for el in num if el.isdigit()]
             except:
+                # Если произошла ошибка, то файл назван некорректно. Тогда просто отбросим его.
                 pass
         return str(sorted(nums)[-1]) if len(nums) > 0 else '-'
 
+@log
 async def main():
     sheeter = Sheet()
     drive = Drive()
@@ -102,9 +100,11 @@ async def main():
     drive_list = await drive.parse(names)
     sheeter.write_values(drive_list, RANGES['drive'], dimension='ROWS')
 
+
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except RuntimeError:
         pass
-
+    except Exception as e:
+        raise e
