70af52ae6e3444a435a5a9d210840cc8b7791c44
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-07-18 14:41:40 +0300

    Fixes

--- fetching.py
+++ fetching.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup as bs
 from browser import Browser
 from selenium.webdriver.common.by import By
 
+from logger import log
 from config import HEADERS, ACCOUNTS
 
 
@@ -32,9 +33,11 @@ class Fetcher:
                 'https://www.bomtoon.com': self.bomtoon,
                 'https://www.comico.kr': self.comico,
                 'https://pocket.shonenmagazine.com': self.shonenmagazine,
+                'https://webtoon.kakao.com': self.webtoon_kakao
             }
         }
 
+    @log
     def get_by_keys(self, dictionary: str, keys: list[str]) -> Any:
         """ Принимает на вход словарь и проходит вглубь по ключам
         :param dictionary: словарь, по которому происходит итерация
@@ -46,6 +49,7 @@ class Fetcher:
             dictionary = dictionary[key]
         return dictionary
 
+    @log
     async def fetch(self, url: str, browser: bool=False) -> str:
         """ Принимает на вход ссылку и возвращает результат парсинга сайта
         :param url: ссылка
@@ -55,13 +59,11 @@ class Fetcher:
         arr = self.sites['browser'] if browser else self.sites['requests']
         for key in arr.keys():
             if key in url:
-                try:
-                    return await arr[key](url) if not browser else arr[key](url)
-                except:
-                    return 'Error'
+                return await arr[key](url) if not browser else arr[key](url)
         else:
-            return 'URL error'
+            return '-'
 
+    @log
     def start_browser(self, user: bool=True, full_load: bool=False) -> None:
         """ Запускает браузер. Если он уже запущен - сначала закроет
         :param user: использовать ли профиль браузера по умолчанию (сохраненные аккаунты)
@@ -70,11 +72,14 @@ class Fetcher:
         if self.driver:
             self.shutdown_browser()
         self.driver = Browser(user=user, full_load=full_load)
+        self.login_kakao()
 
+    @log
     def shutdown_browser(self) -> None:
         """ Закрывает браузер. """
         self.driver.shutdown()
 
+    @log
     async def async_get_response(self, url: str) -> str:
         """ Запрашивает станицу по ссылке, возвращает результат (HTML)
         :param url: ссылка на страницу
@@ -86,6 +91,7 @@ class Fetcher:
                     self.tries = (self.tries - 1) if response.status != 200 else 5
                     return await response.text()
 
+    @log
     def hyperlink(self, url: str, num: Union[str, int]) -> str:
         """ Создает гиперссылку из ссылки и номера
         :param url: ссылка
@@ -94,6 +100,7 @@ class Fetcher:
         """
         return f'=ГИПЕРССЫЛКА("{url}";"{num}")'
 
+    @log
     def get_and_wait(self, url: str, key: str, by: By=By.CLASS_NAME, max_wait: int=10) -> bool:
         """ Прогружает страницу и ждет до тех пор, пока не появится указанный элемент или не истечен время
         :param url: ссылка на страницу
@@ -105,34 +112,37 @@ class Fetcher:
         self.driver.get(url)
         return self.driver.wait_element(by, key, max_wait)
 
+    @staticmethod
+    def format_number(num: str):
+        result = ''.join([el if el.isdigit() else ' ' for el in num])
+        return '.'.join(result.split()) if result else '-'
+
+    @log
     def kakao(self, url: str) -> str:
         if self.get_and_wait(url, 'css-14gr98z'):
             script = "return document.getElementsByClassName('text-ellipsis css-1mn7vax')[0].textContent;"
-            num = self.driver.execute(script).split()[-1][:-1]
-        else:
-            num = '-'
-        return self.hyperlink(url, num)
+            num = self.driver.execute(script, tries=20)
+            return self.hyperlink(url, num[:num.index('화')].split()[-1])
+        return self.hyperlink(url, '-')
 
+    @log
     def series_naver(self, url: str) -> str:
         if 'sortOrder=DESC' not in url:
             url += '&sortOrder=DESC'
-        if self.get_and_wait(url, 'volumeList'):
-            while True:
-                try:
-                    script = "return document.getElementById('volumeList').children[0].getElementsByTagName('strong')[0].textContent;"
-                    num = self.driver.execute(script).split('.')[1]
-                except:
-                    time.sleep(0.2)
-                    continue
-        else:
-            num = '-'
-        return self.hyperlink(url, num)
+        if self.get_and_wait(url, 'volumeList', by=By.ID):
+            script = "return document.getElementById('volumeList').firstChild.getElementsByTagName('strong')[0].textContent;"
+            if res := self.driver.execute(script):
+                res = ''.join([el for el in res.split('.')[0].split()[-1] if el.isdigit()])
+                return self.hyperlink(url, res)
+        return self.hyperlink(url, '-')
 
+    @log
     async def seiga_nicovideo(self, url: str) -> str:
         soup = bs(await self.async_get_response(url), 'lxml')
-        num = len(soup.find('div', {'id': 'episode_list'}).find('ul').find_all('li'))
-        return self.hyperlink(url, num)
+        num = soup.find_all('div', {'class': 'title'})[-1].text
+        return self.hyperlink(url, self.format_number(num))
 
+    @log
     async def web_ace(self, url: str) -> str:
         t_url = url + '/episode/' if url.endswith('/') else url + 'episode/'
         chapter = bs(await self.async_get_response(t_url), 'lxml').find('ul', {'class': 'table-view'}).find_all('li')[0]
@@ -140,18 +150,23 @@ class Fetcher:
         num = ''.join([el for el in num if el.isdigit() or el == '-']).replace('-', '.')
         return self.hyperlink(url, num)
 
+    @log
     async def mechacomic(self, url: str) -> str:
         res = bs(await self.async_get_response(url), 'lxml').find('div', {'class': 'p-search_chapterNo'})
         return self.hyperlink(url, res.findAll('div', {'class', 'u-inlineBlock'})[-1].getText().strip()[1:-2])
 
+    @log
     async def futabanet(self, url: str) -> str:
         from string import punctuation
         res = bs(await self.async_get_response(url), 'lxml').find('div', {'class': 'detail-ex__btn-item--latest'})
         res = res.getText().strip().split('\n')[1][1:]
         res = ''.join([(el if el.isdigit() or el in punctuation else '') for el in res])
+        if '.' in res:
+            res = res[:res.rfind('(')]
         check_bracket = lambda x: '.' if x == '(' else ''
         return self.hyperlink(url, ''.join([el if el.isdigit() else check_bracket(el) for el in res]))
 
+    @log
     async def tappytoon(self, url: str) -> str:
         res = bs(await self.async_get_response(url), 'lxml').find('script', {'id': '__NEXT_DATA__'})
         arr = self.get_by_keys(res.text, ['props', 'initialState', 'entities', 'chapters'])
@@ -160,12 +175,14 @@ class Fetcher:
             if el['isPublished']:
                 return self.hyperlink(url, el['order'])
 
+    @log
     def bilibili(self, url: str) -> str:
         self.driver.get(url)
         self.driver.wait_element(By.CLASS_NAME, 'last-update', 10)
         num = self.driver.execute("return document.getElementsByClassName('last-update')[0].textContent;").split()[1]
         return self.hyperlink(url, num)
 
+    @log
     def bomtoon(self, url: str) -> str:
         self.driver.get(url)
         self.driver.wait_element(By.ID, 'bt-sort-episode', 10)
@@ -174,6 +191,7 @@ class Fetcher:
         res.remove('h0')
         return self.hyperlink(url, len(res))
 
+    @log
     def shonenmagazine(self, url: str) -> str:
         self.driver.get(url)
         time.sleep(5)
@@ -183,24 +201,28 @@ class Fetcher:
         num = ''.join([el if el.isdigit() else ' ' for el in num]).split()[0]
         return self.hyperlink(url, num)
 
+    @log
+    def webtoon_kakao(self, url):
+        import webtoon_kakao
+        return webtoon_kakao.main(url)
+
+    @log
     def ridibooks(self, url: str) -> str:
         self.driver.get(url)
         time.sleep(3)
         if self.driver.driver.current_url.startswith('https://ridibooks.com/account/login?'):
-            self.driver.send_keys_to(By.ID, 'login_id', ACCOUNTS['ridibooks'][0])
-            self.driver.send_keys_to(By.ID, 'login_pw', ACCOUNTS['ridibooks'][1])
-            self.driver.execute("document.getElementsByClassName('account-checkbox')[0].click();")
-            self.driver.execute("document.getElementsByClassName('login-button')[0].click();")
+            self.login_ridibooks()
             time.sleep(5)
             self.driver.get(url)
         if self.driver.wait_element(By.CLASS_NAME, 'book_count', 10):
             # Сколько всего глав на странице
             num = int(self.driver.driver.find_element(By.CLASS_NAME, 'book_count').text.split()[1][:-1])
-            num = self.driver.execute(f"return document.getElementsByClassName('js_book_title')[{num-1}]textContent;")
+            num = self.driver.execute(f"return document.getElementsByClassName('js_book_title')[{num-1}].textContent;")
             num = num.split()[-1][:-1]
         else: num = '-'
         return self.hyperlink(url, num)
 
+    @log
     def comico(self, url: str) -> str:
         self.driver.get(url)
         while not self.driver.wait_element(By.CLASS_NAME, 'btn_kakao', 1, by_driver=True) \
@@ -217,20 +239,41 @@ class Fetcher:
             num = len(bs(self.driver.get_source(), 'lxml').find('ul', {'class': 'list_product'}).findAll('li'))
         return self.hyperlink(url, num)
 
+    @log
     def login_comico(self) -> None:
         s1 = "document.getElementsByClassName('layer_foot')[0].getElementsByTagName('button')[1].click();"
         s2 = "document.getElementsByClassName('btn_kakao')[0].click();"
         self.driver.wait_element(By.CLASS_NAME, 'layer_foot', 15, by_driver=True)
         self.driver.execute(s1 + s2)
-        time.sleep(15)
-        while True:
-            if self.driver.check_element(By.CLASS_NAME, 'list_product', by_driver=True): return True
-            elif 'accounts.kakao.com' in self.driver.driver.current_url:
-                self.driver.send_keys_to(By.ID, 'id_email_2_label', ACCOUNTS['kakao'][0])
-                self.driver.send_keys_to(By.ID, 'id_password_3_label', ACCOUNTS['kakao'][1])
-                self.driver.execute("document.getElementById('staySignedIn').click();")
-                self.driver.execute("document.getElementsByClassName('btn_confirm submit btn_g')[0].click();")
-                time.sleep(5)
-                if 'accounts.kakao.com' in self.driver.driver.current_url: return False
-                else: return True
-            time.sleep(0.2)
+        return self.driver.wait_element(By.CLASS_NAME, 'list_product', 30)
+
+    @log
+    def login_kakao(self) -> None:
+        self.driver.get('https://page.kakao.com/main')
+        if self.driver.wait_element(By.CLASS_NAME, 'css-1urq0jq', 15):
+            time.sleep(10)
+            self.driver.execute('window.stop();')
+            parent = self.driver.driver.current_window_handle
+            self.driver.execute("document.getElementsByClassName('css-1urq0jq')[0].lastChild.click();")
+            time.sleep(3)
+            if len(self.driver.driver.window_handles) == 2:
+                return None
+            # Переключимся на окно авторизации
+            handle = self.driver.driver.window_handles[-1]
+            self.driver.driver.switch_to.window(handle)
+            self.driver.wait_element(By.ID, 'id_email_2', max_wait=15)
+            time.sleep(3)
+            self.driver.send_keys_to(By.ID, 'id_email_2', ACCOUNTS['kakao'][0])
+            self.driver.send_keys_to(By.ID, 'id_password_3', ACCOUNTS['kakao'][1])
+            self.driver.execute("document.getElementById('staySignedIn').click();")
+            time.sleep(0.5)
+            self.driver.execute("document.getElementsByClassName('btn_confirm submit btn_g')[0].click();")
+            time.sleep(5)
+            self.driver.driver.switch_to.window(parent)
+
+    @log
+    def login_ridibooks(self):
+        self.driver.send_keys_to(By.ID, 'login_id', ACCOUNTS['ridibooks'][0])
+        self.driver.send_keys_to(By.ID, 'login_pw', ACCOUNTS['ridibooks'][1])
+        self.driver.execute("document.getElementsByClassName('account-checkbox')[0].click();")
+        self.driver.execute("document.getElementsByClassName('login-button')[0].click();")
