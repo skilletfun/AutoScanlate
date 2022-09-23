3beb46797764d6325550e9bfb8af56d0703707d4
Sergey <skilletfun.laptew.sergey@yandex.ru>
2022-09-23 19:38:53 +0300

    PEP8, type hints

--- logger.py
+++ logger.py
@@ -6,17 +6,17 @@ _log_format = f"%(asctime)s - [%(name)s] - %(message)s"
 
 
 class Logger:
-    def __init__(self, name):
+    def __init__(self, name: str):
         file_handler = logging.FileHandler("logfile.log")
         file_handler.setFormatter(logging.Formatter(_log_format))
         self.logger = logging.getLogger(name)
         self.logger.addHandler(file_handler)
 
-    def log(self, msg):
+    def log(self, msg: str) -> None:
         self.logger.setLevel(logging.INFO)
         self.logger.info(msg)
 
-    def error(self, msg):
+    def error(self, msg: str) -> None:
         self.logger.setLevel(logging.ERROR)
         self.logger.error(msg)
 
