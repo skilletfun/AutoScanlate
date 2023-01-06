9bca82ece58cd7bbead9533090c20a4010658c7d
Sergey <skilletfun.laptew.sergey@yandex.ru>
2023-01-06 16:41:23 +0300

    Update for new version remanga.org

--- remanga.py
+++ remanga.py
@@ -6,45 +6,35 @@ from selenium.webdriver.common.by import By
 from sheet import Sheet
 from browser import Browser
 from logger import log
-from config import RANGES, ACCOUNTS
+from config import RANGES
 
 
 @log
-def login_remanga(driver: Browser) -> None:
-    """ Вход в аккаунт Реманги.
-    :param driver: объект браузера
-    """
-    driver.get('https://remanga.org')
-    time.sleep(5)
-    driver.execute("document.getElementsByClassName('MuiButtonBase-root')[7].click();")
-    time.sleep(3)
-    driver.send_keys_to(By.ID, 'login', ACCOUNTS['remanga'][0])
-    driver.send_keys_to(By.ID, 'password', ACCOUNTS['remanga'][1])
-    driver.execute(
-        "document.getElementsByClassName('MuiButton-containedPrimary MuiButton-containedSizeLarge')[0].click();")
-    time.sleep(5)
-
-@log
-def wait(driver: Browser) -> Tuple(str, list):
+def wait(driver: Browser) -> Tuple[str, list]:
     """ Загружает страницу, пока не появятся необходимые элементы.
     :param driver: объект браузера
     :return: возвращает данные для столбца Реклама и список глав
     """
     timer, tries = 30, 5
+    marketing = '-'
+    children = []
+
     while True:
         try:
             # Если тайтл не принадлежит аккаунту, то вернется текст Читать / Прочитано / Продолжить
-            status = driver.execute("return document.getElementsByClassName('MuiButton-label')[2].textContent;")
-            if 'Продвигается' in status or 'В очереди' in status:
-                marketing = status.split()[-1]
-            elif 'Продвижение' in status:
-                marketing = 'Продвижение'
-            elif 'Нет глав' in status:
-                return '-', []
-            else:
-                marketing = '-'
-            children = driver.execute("return document.getElementsByClassName('MuiGrid-grid-md-6')[0].lastChild.children;")[1:]
-            children[0].find_element(By.CSS_SELECTOR, 'div:nth-child(3)')
+            status = driver.execute("return document.getElementsByClassName('Button_label__py8bK')[1].textContent;")
+            # Если элемент существует, то True, если не существует False
+            if status:
+                if 'Продвигается' in status or 'В очереди' in status:
+                    marketing = status.split()[-1]
+                elif 'Продвижение' in status:
+                    marketing = 'Продвижение'
+
+            flag_chapters_count = driver.execute("return document.getElementsByClassName('Tabs_flexContainer__Zbnn6')[0].children[1].textContent;")
+            if flag_chapters_count and not flag_chapters_count.endswith('(0)'):
+                # Возвращает список элементов-глав, откуда будут вытащены параметры глав для таблицы
+                while not children:
+                    children = driver.execute("return document.getElementsByClassName('Chapters_container__G5BYo')[1].children;")
             break
         except:
             time.sleep(0.1)
@@ -52,7 +42,7 @@ def wait(driver: Browser) -> Tuple(str, list):
             if timer < 0:
                 timer, tries = 30, tries-1
                 if tries < 0: return '-', []
-                driver.driver.refresh()
+                driver.refresh()
     return marketing, children
 
 @log
@@ -60,30 +50,37 @@ def main():
     sheeter = Sheet()
     urls = sheeter.get_values('C4:C1000')[0]
     driver = Browser(full_load=True)
+
     try:
         result_arr = []
         for url in urls:
             if url.startswith('http'):
                 url = url.replace('?subpath=about', '')
-                payed, free, marketing, date = '-', '-', '-', '-'
-                total_payed = 0
+                url = url.replace('?subpath=content', '')
+                if not url.endswith('?p=content'):
+                    url += '?p=content'
+
+                payed = free = date = '-'   # Платка, бесплатка и дата для таблицы
+                total_payed = 0             # Всего платок для таблицы
 
                 driver.get(url)
-
+                time.sleep(1)
                 marketing, children = wait(driver)
+
                 for child in children:
-                    # Возвращает кусок текста, по которому точно можно сказать, платная или бесплатная глава
-                    el = child.find_element(By.CSS_SELECTOR, 'div:nth-child(3)').get_attribute('innerHTML')[2:5]
+                    # True если платная, False если бесплатная
+                    chapter_type_flag = driver.execute("return arguments[0].getElementsByTagName('svg')[0].hasAttribute('aria-label');", arg=child)
                     # Дата главы
                     if date == '-':
-                        date = child.find_element(By.CSS_SELECTOR, 'a > div > span:nth-child(2)').get_attribute('innerHTML')
+                        date = driver.execute("return arguments[0].getElementsByTagName('p')[1].textContent;", arg=child).replace('/', '.')
                     # Номер главы
-                    num = child.find_element(By.CSS_SELECTOR, 'a > h6').text.split()[1]
+                    num = driver.execute("return arguments[0].getElementsByTagName('p')[0].textContent;", arg=child).split()[1]
 
-                    if el.startswith('v'):
+                    if chapter_type_flag:
                         total_payed += 1
-                        if payed == '-': payed = f'=ГИПЕРССЫЛКА("{url}";"{num}")'
-                    elif el.startswith('p'):
+                        if payed == '-':
+                            payed = f'=ГИПЕРССЫЛКА("{url}";"{num}")'
+                    else:
                         free = f'=ГИПЕРССЫЛКА("{url}";"{num}")'
                         break
                 result_arr.append([payed, free, total_payed, date, marketing])
@@ -94,7 +91,6 @@ def main():
         raise e
     finally:
         driver.shutdown()
-
 
 if __name__ == '__main__':
     main()
