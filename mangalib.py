d891c468aaccaf174c53a948972b0c4e05110529
Sergey <skilletfun.laptew.sergey@yandex.ru>
2023-01-30 16:05:48 +0300

    Refactor

--- mangalib.py
+++ mangalib.py
@@ -27,10 +27,11 @@ def get_chapter(driver: Browser) -> str:
 @log
 def load_url(driver: Browser, url: str) -> str:
     """ Возвращает номер последней загруженной на сайт главы, если она есть.
-     В противном случае возвращает прочерк. Максимум на прогрузку страницы - total_time
+     В противном случае возвращает прочерк.
+     Максимум секунд на прогрузку страницы - total_time
      """
     total_time = 5
-    driver.get(url.replace('mangalib.me', 'mangalib.org'))  # Потому что к me - access denied
+    driver.get(url.replace('mangalib.me', 'mangalib.org'))  # Потому что к .me - access denied
     while True:
         if driver.check_element(By.CLASS_NAME, 'media-chapter__name', by_driver=True):
             return f'=ГИПЕРССЫЛКА("{url}";"{get_chapter(driver)}")'
@@ -41,22 +42,13 @@ def load_url(driver: Browser, url: str) -> str:
             if total_time < 0:
                 return f'=ГИПЕРССЫЛКА("{url}";"-")'
             sleep(0.2)
-            continue
 
 @log
 def main():
-    sheeter = Sheet()
-    urls = sheeter.get_values('D4:D1000')[0]
-    driver = Browser(user=False)
-    try:
-        result_arr = []
-        for url in urls:
-            result_arr.append(load_url(driver, url))
+    with Browser(user=False) as driver:
+        sheeter = Sheet()
+        result_arr = [load_url(driver, url) for url in sheeter.get_values('D4:D1000')[0]]
         sheeter.write_values([result_arr], RANGES['mangalib'])
-    except Exception as e:
-        raise e
-    finally:
-        driver.shutdown()
 
 
 if __name__ == '__main__':
