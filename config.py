1cfc4c5d2c98128d1b9ad6cedcf8993af62355c2
Sergey <skilletfun.laptew.sergey@yandex.ru>
2023-02-01 20:17:35 +0300

    Update structure with other changes

--- config.py
+++ config.py
@@ -1,23 +1,11 @@
 # Установить значение False, если продакшн
 # При True запись в таблицу происходить не будет, результат будет выводиться в консоль
-DEBUG = False
-
-# Аккаунты, которые будут использоваться в работе
-ACCOUNTS = {
-    'kakao': ('login', 'pass'),
-    'ridibooks': ('login', 'pass')
-}
-
-# В какие аккаунты потребуется авторизация (True - требуется, False - не требуется)
-LOGIN_TO = {
-    'KAKAO': True,
-    'RIDIBOOKS': True,
-    'COMICO': True
-}
+DEBUG = True
 
 # Диапазоны данных в таблице для записи
+# Если данные не на первом листе, то указать сначала название листа с воскл. знаком (Лист!А1:А1000)
 RANGES = {
-    'original': 'F4:F1000',
+    'original': 'G4:G1000',
     'remanga': 'H4:L1000',
     'mangalib': 'L4:L1000',
     'vk': 'O4:P1000',
@@ -25,28 +13,31 @@ RANGES = {
 }
 
 # Диапазоны исходных даных в таблице
+# Если данные не на первом листе, то указать сначала название листа с воскл. знаком (Лист!А1:А1000)
 SOURCE_RANGES = {
     'names': 'A4:A1000',
     'original': 'B4:B1000',
     'remanga': 'C4:C1000',
     'mangalib': 'D4:D1000',
     'vk': 'E4:E1000',
-    'genres': 'S4:S1000'
+    'genres': 'S4:S1000',
+    'tests': 'TestData!A1:A999'
 }
 
-# Токен вк для парсинга обсуждений группы
+# Токен вк для парсинга обсуждений в группе
 VK_API_TOKEN = ''
 
-# Путь до браузерной папки User data
+# Путь до браузерной папки User Data
 PATH_TO_BROWSER = 'C:\\Users\\User\\AppData\\Local\\Chromium\\User Data'
 
-# ID таблицы
+# ID Google-таблицы
 SHEET_ID = ''
 
-# Максимальное количество запросов к диску в секунду
+# Максимальное количество запросов к Google-диску в секунду
+# При значении выше плюс минус 30 Google сбрасывает соединение
 DRIVE_FETCH_LIMIT = 15
 
-# Название и ID гугл-папок, по которым будет происходить поиск
+# Название и ID Google-папок с жанрами, по которым будет происходить поиск
 DRIVE_SCOPES = {
     'folder_name_1': 'folder_id_1',
     'folder_name_2': 'folder_id_2',
@@ -67,3 +58,10 @@ HEADERS = {
     "Connection": "keep-alive",
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 }
+
+# С какими параметрами будет запущена сессия
+ARGS_FOR_BROWSER = [
+    '--disable-features=VizDisplayCompositor',
+    '--blink-settings=imagesEnabled=false',
+    '--ignore-certificate-errors-spki-list'
+]
